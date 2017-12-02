b27db9f0eef6121650503b2d173a86e2e5a80af0
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-02 18:25:15 +0100

    Implemented a crappy version of a prefab brush

---
+++ Game/Assets/Editor/Brushes/PrefabBrush.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// The most shitties brush ever, todo research brushes later
+[CustomGridBrush(true, false, false, "PrefabBrush Brush")]
+public class PrefabBrush : GridBrush
+{
+    [SerializeField] private GameObject _prefab;
+
+    public override void Paint(GridLayout grid, GameObject brushTarget, Vector3Int position)
+    {
+        if (_prefab == null)
+            return;
+
+        var correctedPosition = position + Vector3.one * 0.5f;
+
+        var possibleHit = Physics2D.OverlapPoint(correctedPosition);
+        if (possibleHit != null)
+            return;
+
+        var instance = Instantiate(_prefab);
+        instance.transform.position = correctedPosition;
+    }
+}
