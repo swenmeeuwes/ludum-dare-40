71c77b56b5b6ca62a83d14d75125164e8b863580
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-02 20:58:50 +0100

    Gave the fireball some cool particle effects

--- Game/Assets/Scripts/Spells/SpellProducts/Fireball.cs
+++ Game/Assets/Scripts/Spells/SpellProducts/Fireball.cs
@@ -5,5 +5,40 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Fireball : MonoBehaviour
 {
-    //todo    
+    private ParticleSystem _particleSystem;
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _particleSystem.Play();
+    }
+
+    private void Start()
+    {        
+        //StartCoroutine(PlayParticleSystemDelayed()); // Not needed anymore?
+    }        
+
+    // Quick hack to prevent particles from spawning at Vector3.zero in world pos on first tick
+    private IEnumerator PlayParticleSystemDelayed()
+    {
+        yield return null;
+
+        _particleSystem.Play();
+    }
+
+    public void Sleep()
+    {
+        StopAllCoroutines();
+        _rigidbody.Sleep();        
+        _particleSystem.Stop();        
+    }
+
+    public void WakeUp()
+    {
+        _rigidbody.WakeUp();
+        StartCoroutine(PlayParticleSystemDelayed());
+    }
 }
