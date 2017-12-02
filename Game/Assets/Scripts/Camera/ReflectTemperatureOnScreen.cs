a7af6dd72eff3a3d240ac4d8bc2f2589c9d90aa3
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-02 18:25:03 +0100

    Implemented a temperature reflection shader

---
+++ Game/Assets/Scripts/Camera/ReflectTemperatureOnScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectTemperatureOnScreen : MonoBehaviour {
+    [SerializeField] private Material _fadeToTemperatureColorMaterial;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_fadeToTemperatureColorMaterial != null && TemperatureManager.Instance != null)
+        {
+            _fadeToTemperatureColorMaterial.SetFloat("_Temperature", TemperatureManager.Instance.Temperature);
+            Graphics.Blit(source, destination, _fadeToTemperatureColorMaterial);
+        }
+    }
+}
