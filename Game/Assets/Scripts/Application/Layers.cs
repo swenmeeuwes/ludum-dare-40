351efc316f6e233ab3f499b1a0337d5ed3b93505
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-02 15:35:32 +0100

    Defined layers

---
+++ Game/Assets/Scripts/Application/Layers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Layers {
+    Default = 0,
+    TransparentFx = 1,
+    IgnoreRaycast = 2,
+    Water = 4,
+    Ui = 5,
+    Player = 8,
+    PlayerSpells = 9
+}
