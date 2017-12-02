cd60a95bf74cf7908750ac9a77771fb19d835742
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-02 14:03:03 +0100

    Implemented temperature ui + controller Removed ground check since it was replaced by collision check

---
+++ Game/Assets/Scripts/UI/TemperatureUIController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemperatureUIController : MonoBehaviour
+{    
+    [SerializeField] private Slider _temperatureSlider;
+    [SerializeField] private Color _coldColor;
+    [SerializeField] private Color _hotColor;
+
+    private void Start()
+    {
+        _temperatureSlider.onValueChanged.AddListener(UpdateTemperatureSliderColor);
+    }
+
+    private void OnDestroy()
+    {
+        _temperatureSlider.onValueChanged.RemoveListener(UpdateTemperatureSliderColor);
+    }
+
+    private void Update()
+    {
+        if (TemperatureManager.Instance != null)
+            _temperatureSlider.value = TemperatureManager.Instance.Temperature;
+    }
+
+    private void UpdateTemperatureSliderColor(float newValue)
+    {
+        _temperatureSlider.fillRect.gameObject.GetComponent<Image>().color = Color.Lerp(_coldColor, _hotColor, newValue);
+    }
+}
