d4da266d20b5c2275dbce19fbb737746d4d79465
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-02 14:02:14 +0100

    Implemented temperature manager

---
+++ Game/Assets/Scripts/Application/TemperatureManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemperatureManager : MonoSingleton<TemperatureManager>
+{
+    [Tooltip("Scales from 0 to 1, where 0 is cold and 1 hot")][Range(0, 1)] public float Temperature;
+
+    private void Awake()
+    {
+        DefineSingleton(this);
+    }
+}
