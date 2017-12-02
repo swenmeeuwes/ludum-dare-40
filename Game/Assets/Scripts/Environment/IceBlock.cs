148e1faac15a435215109fb077699ab75d354321
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-02 16:02:46 +0100

    Implemented an ice block that can melt/ freeze depending on the environment, it also holds an item

---
+++ Game/Assets/Scripts/Environment/IceBlock.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class IceBlock : MonoBehaviour
+{
+    [SerializeField] private float _freezeThreshold;
+    [SerializeField] private float _meltThreshold;
+    [SerializeField] private float _secondsTillStateChange;
+
+    public GameObject Holding;
+
+    private float _health;
+    private float _startOpacity;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _health = _secondsTillStateChange;        
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _startOpacity = _spriteRenderer.color.a;
+
+        ReleaseHoldingObject(false);
+    }
+
+    private void Update()
+    {
+        if (TemperatureManager.Instance != null)
+        {
+            if (TemperatureManager.Instance.Temperature < _freezeThreshold)
+                AddHealth(Time.deltaTime);
+            if (TemperatureManager.Instance.Temperature > _meltThreshold)
+                AddHealth(-Time.deltaTime);
+        }
+
+        if (_health <= 0)
+        {
+            ReleaseHoldingObject(true);
+            Destroy(gameObject);
+        }
+    }
+
+    private void AddHealth(float amount)
+    {
+        _health = Mathf.Clamp(_health + amount, 0, _secondsTillStateChange);
+
+        var newColor = _spriteRenderer.color;
+        newColor.a = (_health / _secondsTillStateChange) * _startOpacity;
+
+        _spriteRenderer.color = newColor;
+    }
+
+    private void ReleaseHoldingObject(bool release)
+    {
+        if (Holding != null)
+        {
+            Holding.transform.position = transform.position;
+
+            var holdingColliders = Holding.GetComponents<Collider2D>();
+            foreach (var collider in holdingColliders)
+            {
+                collider.enabled = release;
+            }
+
+            var holdingRigidbody = Holding.GetComponent<Rigidbody2D>();
+            if (holdingRigidbody)
+            {
+                if (release)
+                    holdingRigidbody.WakeUp();
+                else
+                    holdingRigidbody.Sleep();
+            }
+        }
+    }
+}
