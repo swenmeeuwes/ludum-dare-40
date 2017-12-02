5f91d3f7a5d4667dfbfcc3a37937a7b8fe80c53a
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-02 12:16:54 +0100

    Implemented camera fade

---
+++ Game/Assets/Scripts/Camera/FadeToColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FadeToColor : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private Material _fadeToColorMaterial;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_fadeToColorMaterial != null)
+            Graphics.Blit(source, destination, _fadeToColorMaterial);
+    }
+
+    private void LateUpdate()
+    {
+        _fadeToColorMaterial.SetFloat("_Fade", _player.Energy / _player.MaxEnergy);
+    }
+}
---
+++ Game/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] public float _initialMaxEnergy;
+    [Tooltip("Energy usage per second while moving")][SerializeField] private float _energyUsage;
+
+    public float MaxEnergy { get; private set; }
+    public float Energy { get; private set; }
+
+    private PlayerMovement _movement;
+
+    private void Awake()
+    {
+        _movement = GetComponent<PlayerMovement>();
+
+        MaxEnergy = _initialMaxEnergy;
+        Energy = _initialMaxEnergy;
+    }
+
+    private void LateUpdate()
+    {
+        if(_movement.IsMoving)
+            AddEnergy(-_energyUsage * Time.deltaTime);
+        else
+            AddEnergy(_energyUsage * Time.deltaTime);
+    }
+
+    private void AddEnergy(float addition)
+    {
+        Energy = Mathf.Clamp(Energy + addition, 0, MaxEnergy);
+    }
+}
