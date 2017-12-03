e3259270ace23c248b75b4701507b8e01e4ea5c6
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-03 01:26:22 +0100

    Implemented first iteration tutorial player

---
+++ Game/Assets/Scripts/Tutorial/TutorialItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TutorialItemActionType
+{
+    None,
+    ShowTemperatureMeter
+}
+
+[Serializable]
+public class TutorialItem
+{
+    public string Text;
+    public TutorialItemActionType Action;
+}
---
+++ Game/Assets/Scripts/Tutorial/TutorialPlayer.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPlayer : MonoBehaviour
+{
+    [SerializeField] private TypeWriter _tutorialTextField;
+    [SerializeField] private Text _continueTextField;
+    [SerializeField] private float _tutorialDelay;
+
+    [Tooltip("Time in seconds before the tutorial starts")] public TutorialItem[] Sequence;
+
+    public bool IsFinished { get; set; }
+
+    public TutorialItem CurrentSequenceItem
+    {
+        get { return Sequence[_sequenceIndex]; }
+    }
+
+    public TutorialItem NextSequenceItem
+    {
+        get
+        {
+            _sequenceIndex++;
+            return _sequenceIndex > Sequence.Length - 1 ? null : CurrentSequenceItem;
+        }
+    }
+
+    private Player _player;
+    private int _sequenceIndex;
+
+    private void Awake()
+    {
+        _sequenceIndex = -1;
+        _continueTextField.enabled = false;        
+    }
+
+    private void Start()
+    {
+        _player = FindObjectOfType<Player>();
+        _player.Movement.enabled = false;
+        _player.Wand.enabled = false;
+
+        _tutorialTextField.AddEventListener(TypeWriter.Finished, OnTypeWriterFinished);
+
+        Invoke("Next", _tutorialDelay);
+    }
+
+    private void OnDestroy()
+    {
+        _tutorialTextField.RemoveEventListener(TypeWriter.Finished, OnTypeWriterFinished);
+    }
+
+    private void Update()
+    {
+        if (IsFinished)
+            return;
+
+        if (Input.anyKeyDown)
+        {
+            if (_tutorialTextField.IsWriting)
+                _tutorialTextField.Finish();
+            else
+                Next();            
+        }
+    }
+
+    private void Next()
+    {        
+        if (_sequenceIndex + 1 > Sequence.Length - 1 && !IsFinished)
+        {
+            StartCoroutine(Finish());
+            return;
+        }
+
+        _continueTextField.enabled = false;
+        HandleTutorialItem(NextSequenceItem);
+    }
+
+    private IEnumerator Finish()
+    {
+        IsFinished = true;        
+
+        _continueTextField.enabled = false;
+        _tutorialTextField.GetComponent<Text>().enabled = false;
+
+        yield return new WaitForSeconds(0.5f);
+
+        _player.Movement.enabled = true;
+        _player.Wand.enabled = true;
+    }
+
+    private void HandleTutorialItem(TutorialItem tutorialItem)
+    {
+        _tutorialTextField.Type(tutorialItem.Text);
+    }
+
+    private void OnTypeWriterFinished(EventObject eventObject)
+    {
+        _continueTextField.enabled = true;
+    }
+}
