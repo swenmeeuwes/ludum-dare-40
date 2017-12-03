4cbd31ca7196d049545441897a1e70defb847a19
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-03 02:58:19 +0100

    Implemented tag trigger

---
+++ Game/Assets/Scripts/Triggers/TagTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider2D))]
+public class TagTrigger : MonoBehaviour
+{
+    [SerializeField] private Tags _tag;
+    [SerializeField] private UnityEvent _onTrigger;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == _tag.ToString())
+            _onTrigger.Invoke();
+    }
+}
