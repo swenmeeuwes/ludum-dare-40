889cd5443b9f5f8b12e95f81e75e718b34d8fd21
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-03 20:33:59 +0100

    Implemented heavy object

---
+++ Game/Assets/Scripts/Entity/HeavyObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class HeavyObject : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;    
+    private Collider2D _collider;    
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_rigidbody.velocity.magnitude < 2f)
+            return;
+
+        var player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            player.Hit();
+            gameObject.layer = (int) Layers.Deactivated;
+        }
+    }
+}
