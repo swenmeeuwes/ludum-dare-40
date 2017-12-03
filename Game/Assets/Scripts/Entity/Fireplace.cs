a44ef8536dc04d29520dda49602bb4435f8ef9b1
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-03 22:52:08 +0100

    Implemented fireplaces

---
+++ Game/Assets/Scripts/Entity/Fireplace.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireplace : MonoBehaviour
+{
+    [Tooltip("Temperature to warm to")]
+    [Range(0, 1)]
+    [SerializeField] private float _warmTill = 0.6f;
+
+    private ParticleSystem _particleSystem;
+
+    private void Awake()
+    {
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
+        if (_particleSystem != null)
+            _particleSystem.Stop();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var player = other.GetComponent<Player>();
+        if (player != null && TemperatureManager.Instance != null &&
+            TemperatureManager.Instance.Temperature < _warmTill)
+        {
+            TemperatureManager.Instance.SetTemperature(_warmTill);
+            if (_particleSystem != null)
+                _particleSystem.Play();
+        }
+    }
+}
