ab71bbdef132173c8ed6d9e52c84fd2446ac1355
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-03 15:14:53 +0100

    Implemented respawn trigger

---
+++ Game/Assets/Scripts/Triggers/RespawnTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnTrigger : TagTrigger
+{
+    [SerializeField] private Vector3 _respawnPoint;
+
+    protected override void Trigger(Collider2D other)
+    {
+        base.Trigger(other);
+
+        other.transform.position = _respawnPoint;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(_respawnPoint, 0.5f);
+    }
+}
--- Game/Assets/Scripts/Triggers/TagTrigger.cs
+++ Game/Assets/Scripts/Triggers/TagTrigger.cs
@@ -6,12 +6,17 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Collider2D))]
 public class TagTrigger : MonoBehaviour
 {
-    [SerializeField] private Tags _tag;
-    [SerializeField] private UnityEvent _onTrigger;
+    [SerializeField] protected Tags Tag;
+    [SerializeField] protected UnityEvent OnTrigger;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    protected void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == _tag.ToString())
-            _onTrigger.Invoke();
+        if (other.tag == Tag.ToString())
+            Trigger(other);
+    }
+
+    protected virtual void Trigger(Collider2D other)
+    {
+        OnTrigger.Invoke();
     }
 }
