c23894d670339ed605dfe9f7ffd51e3f7b7d3a83
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-03 22:51:26 +0100

    Finished level 2

---
+++ Game/Assets/Scripts/Util/ControlGameObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlGameObject : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _controlledGameObjects;
+    [SerializeField] private bool _disableByDefault = true;
+
+    private void Awake()
+    {
+        if (_disableByDefault)
+            DisableGameObjects();
+    }
+
+    public void DisableGameObjects()
+    {
+        foreach (var controlledGameObject in _controlledGameObjects)
+        {
+            controlledGameObject.SetActive(false);
+        }            
+    }
+
+    public void EnableGameObjects()
+    {
+        foreach (var controlledGameObject in _controlledGameObjects)
+        {
+            controlledGameObject.SetActive(true);
+        }
+    }
+}
