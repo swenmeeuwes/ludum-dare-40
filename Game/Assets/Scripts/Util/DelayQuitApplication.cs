c219cf46f058cd9c9bd096ff41071c559af8642e
Swen Meeuwes <swenmeeu@gmail.com>
2017-12-04 03:18:44 +0100

    Implemented auto quit

---
+++ Game/Assets/Scripts/Util/DelayQuitApplication.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayQuitApplication : MonoBehaviour {
+    [SerializeField] private float _secondsTillQuit;
+
+    private void Awake()
+    {
+        Invoke("Quit", _secondsTillQuit);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
